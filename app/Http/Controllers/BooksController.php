f33cc6814f26b9fc5b448d7cf74134e26fc06b47
leffalt <leffalt@gmail.com>
2020-09-10 17:59:32 +0700

    Add BooksController

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+/**
+ * Class BooksController
+ * @package App\Http\Controllers
+ */
+
+class BooksController
+{
+    /**
+     * GET /books
+     * @return array
+     */
+    public function index(): array
+    {
+        return [];
+    }
+}
